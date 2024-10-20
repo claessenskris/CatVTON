db2d4ff7c53449968957054285dac31f3761cb4d
Kris Claessens <59504265+claessenskris@users.noreply.github.com>
2024-10-20 16:25:06 +0200

    Add files via upload

---
+++ installation/CatVTON.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gcsfuse --implicit-dirs image_generator_request /pascalem/_pyt/CatVTON/data
+
+cd /pascalem
+
+source _pyt/bin/activate
+
+# Check if the environment is activated
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment not activated."
+    exit 1
+fi
+
+cd _pyt/CatVTON
+
+python3 FileAppRun.py --run_id CatVTON --bucket image_generator_request --config _config1 --token confident-coder-285618-1a0704c69644.json 
